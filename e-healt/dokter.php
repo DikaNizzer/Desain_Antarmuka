8b8ec9c3bda0feadabcc11bc5e40d25fada06eb6
hiizain <hizain512@gmail.com>
2022-06-01 19:40:29 +0700

    hilmi/1June2022
    Header, BG Landing Page

---
+++ e-healt/dokter.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Progate</title>
+    <link rel="stylesheet" href="../assets/stylesheet.css">
+    <link rel="stylesheet" href="responsive.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-top">
+            <p>Jl. Airlangga No.4 - 6, Airlangga, Kota Surabaya.</p>
+            <p>+62 81234567</p>
+        </div>
+        <div class="header-right">
+            <hr>
+            <a href="#">Beranda</a>
+            <a href="#">Profil</a>
+            <a href="#">Fasilitas</a>
+            <a href="#">Pelayanan</a>
+            <a href="#">Berita</a>
+            <a href="#">Promosi</a>
+            <a href="#">Kontak</a>
+        </div>
+      </div>
+    </header>
+    <div class="content">
+        <div class="container">
+            <div class="tabs">
+                <div class="tab">
+                    <a href="">Jadwal</a>
+                </div>
+                <div class="tab">
+                    <a href="">Dokter</a>
+                </div>
+            </div>    
+        </div>
+    </div>
+
+
+    
+    <footer>
+      <div class="container">
+        <img src="https://prog-8.com/images/html/advanced/footer_logo.png">
+        <p>Learn to code, learn to be creative.</p>
+      </div>
+    </footer>
+  </body>
+</html>
--- e-healt/landing/landing.php
+++ e-healt/landing/landing.php
@@ -5,11 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E - Healt</title>
-    <link rel="stylesheet" href="../../assets/style.css">
+    <link rel="stylesheet" href="../../assets/stylesheet.css">
 </head>
-<body class="bg">
+    <body>
+        <header>
+            <div class="container">
+                <div class="header-top">
+                    <p>Jl. Airlangga No.4 - 6, Airlangga, Kota Surabaya.</p>
+                    <p>+62 81234567</p>
+                </div>
+                <div class="header-right">
+                    <hr>
+                    <a href="#">Beranda</a>
+                    <a href="#">Profil</a>
+                    <a href="#">Fasilitas</a>
+                    <a href="#">Pelayanan</a>
+                    <a href="#">Berita</a>
+                    <a href="#">Promosi</a>
+                    <a href="#">Kontak</a>
+                </div>
+            </div>
+        </header>
+        <div class="top-wrapper">
+            <div class="container">
+            </div>
+        </div>
 
-    <!-- <div class="bg"></div> -->
-    
-</body>
+
+        
+        <footer>
+            <div class="container">
+                <img src="https://prog-8.com/images/html/advanced/footer_logo.png">
+                <p>Learn to code, learn to be creative.</p>
+            </div>
+        </footer>
+    </body>
 </html>

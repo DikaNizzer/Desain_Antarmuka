df9771a3fa8809477b453b51d2e8a505158badd9
dikanizzer <dikanizzer29@gmail.com>
2022-06-06 20:00:13 +0700

    form check

--- e-healt/form_daftar/cek.php
+++ e-healt/form_daftar/cek.php
@@ -6,10 +6,107 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E - Healt</title>
     <link rel="stylesheet" href="../../assets/stylesheet.css">
+
+
+    <!-- CSS Form -->
+    <style>
+* {
+  box-sizing: border-box;
+}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #D7B94C;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+input[type=submit] {
+  background-color: #D7B94C;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-bottom: 25px;;
+}
+
+input[type=submit]:hover {
+  background-color: #002B7D;
+  
+}
+
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+
+
+/* CSS MODAL */
+#example {
+        visibility: hidden;
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        z-index: 1000;
+      }
+      #example div {
+        width: 600px;
+        height: 300px;
+        margin: 150px auto;
+        background-color: #f2f2f2;
+        border-radius: 10px;
+        -webkit-border-radius: 10px;
+        -moz-border-radius: 10px;
+        border: 1px solid #666666;
+        padding: 15px;
+        text-align: center;
+        font-weight: bold;
+        font-size: 15px;
+        border: 3px solid #cccccc;
+        position: absolute;
+        left: 50%;
+        top: 100px;
+        transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        -webkit-transform: translate(-50%, -50%);
+      }
+</style>
 </head>
-    <body >
-        <header2>
-            <div class="container">
+    <body>
+        <header style="height: 425px; position:relative;">
+            <div class="container" >
                 <div class="header-top">
                     <p>Jl. Airlangga No.4 - 6, Airlangga, Kota Surabaya.</p>
                     <p>+62 81234567</p>
@@ -25,22 +122,76 @@
                     <a href="#">Kontak</a>
                 </div>
             </div>
-        </header2>
+        </header>
+        <!-- <div class="top-wrapper" style="height: 340px;">
+            <div class="container" style="height: 340px;">
+            </div>
+        </div> -->
 
-        <div class="pembatas">
-            <!-- <div class="container"></div> -->
+        <div class="pembatas"></div>
+        <center><h1>Pendaftaran Layanan</h1></center>
+
+        
+        <div class="container">
+            <form action="/action_page.php">
+                <div class="row">
+                    <div class="col-25">
+                        <label for="fname">NIK</label>
+                    </div>
+                    <div class="col-75">
+                        <input type="text" id="fname" name="firstname" placeholder="Masukkan NIK sesuai KK">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-25">
+                        <label for="lname">Nama</label>
+                    </div>
+                    <div class="col-75">
+                        <input type="text" id="lname" name="lastname" placeholder="Masukkan nama lengkap sesuai KK">
+                    </div>
+                </div>
+
+            <div class="row">
+                <div class="col-25">
+                    <label for="subject">No Hp</label>
+                </div>
+                <div class="col-75">
+                <input type="text" id="lname" name="lastname" placeholder="+62">
+                </div>
+            </div>
+        
+
+        <div class="row" style="margin-top: 5px ;">
+            <div class="col-25">
+                    <!-- <label for="subject">No Hp</label> -->
+            </div>
+            <div class="col-75">
+            <p style="color:red; font-style: oblique;">Pastikan data telah terisi dengan benar !</p>
+            </div>
+        </div>
+
+        <br>
+            <!-- <div class="row"> -->
+                <center><input type="submit" value="Submit"></center>
+            <!-- </div> -->
+            </form>
         </div>
         
-        <div class="container" style="margin-top:460px">
-            <center><h1>Pendaftaran Layanan</h1></center>
+        
+
+        <a href='#' onclick='example()'>open</a>
+        <div id="example">
+            <div>
+                <h3>Data Tidak Ditemukan !</h3>
+                <center><img src='../../assets/bg/folder.png' style="width:200px; height:200px; align:center; position:relative;"></center>
+                <a href='#' onclick='example()'>Click here to close the box</a>
+            </div>
+            <script>
+                function example() {
+                el = document.getElementById("example");
+                el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+                }
+            </script>
         </div>
-
-        
-
-        
-        
-
-        
-        
     </body>
 </html>
